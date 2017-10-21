14e554805798c909b27dd1714af301b2a170091d
Daniel Gardner <bowshanks@gmail.com>
2017-10-21 10:49:46 -0600

    test to functioning draft 1

--- js/controllers/testCtrl.js
+++ js/controllers/testCtrl.js
@@ -1,5 +1,19 @@
 angular.module('jamitup').controller('testCtrl',function($scope, $location){
   $scope.isLoading = true;
+  $scope.tag = document.getElementById('link');
+  console.log($scope.tag);
+
+
+  getSMSUrl()
+  .then(function(value) {
+    $scope.isLoading = false;
+    $scope.url = value;
+    $scope.tag.setAttribute("href", value);
+    location.href = $scope.url;
+    $scope.$apply();
+  }, function(reason) {
+    console.error('getSMSUrl',reason);
+  });
 
   function getSMSUrl() {
     return new Promise(function(resolve, reject) {
@@ -14,17 +28,29 @@ angular.module('jamitup').controller('testCtrl',function($scope, $location){
           {
             url = "sms:1-408-555-1212?body=" + encodeURIComponent("HELLO WORLD");
           }
-      resolve(url);
+
+      setTimeout(setCountDown3,800);
+      setTimeout(setCountDown2,1600);
+      setTimeout(setCountDown1,2400);
+      setTimeout(function(){
+        resolve(url);
+      },2600);
     });
   }
 
-  getSMSUrl()
-  .then(function(value) {
-    $scope.isLoading = false;
-    $scope.url = value
-    location.href = $scope.url;
-  }, function(reason) {
-    console.error('getSMSUrl',reason);
-  })
+  function setCountDown3() {
+    $scope.countDown3 = {'color': 'black'}
+    $scope.$apply();
+  }
+
+  function setCountDown2() {
+    $scope.countDown2 = {'color': 'black'}
+    $scope.$apply();
+  }
+
+  function setCountDown1() {
+    $scope.countDown1 = {'color': 'black'}
+    $scope.$apply();
+  }
 
 });
