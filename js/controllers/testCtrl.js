a4ab2663d61290b2ff96890d9f5d805edfa155a8
Daniel Gardner <bowshanks@gmail.com>
2017-10-20 19:48:58 -0600

    testing auto load

--- js/controllers/testCtrl.js
+++ js/controllers/testCtrl.js
@@ -1,3 +1,30 @@
 angular.module('jamitup').controller('testCtrl',function($scope, $location){
-  console.log('we in business');
+  $scope.isLoading = true;
+
+  function getSMSUrl() {
+    return new Promise(function(resolve, reject) {
+      var ua = navigator.userAgent.toLowerCase();
+      var url;
+
+      if (ua.indexOf("iphone") > -1 || ua.indexOf("ipad") > -1)
+          {
+            url = "sms:1-408-555-1212;body=" + encodeURIComponent("I'm at ");
+          }
+      else
+          {
+            url = "sms:1-408-555-1212?body=" + encodeURIComponent("I'm at ");
+          }
+      resolve(url);
+    });
+  }
+
+  getSMSUrl()
+  .then(function(value) {
+    $scope.isLoading = false;
+    $scope.url = value
+    location.href = $scope.url;
+  }, function(reason) {
+    console.error('getSMSUrl',reason);
+  })
+
 });
